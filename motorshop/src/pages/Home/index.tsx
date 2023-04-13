2bb1edfb35b4064424757d1a1c36052a2a5362fa
Enrico Vieira <enricovieira@hotmail.com>
2023-04-13 18:58:45 -0300

    feat: add carshowcase and fix home overlay

--- motorshop/src/pages/Home/index.tsx
+++ motorshop/src/pages/Home/index.tsx
@@ -1,52 +1,13 @@
-import CarCard from "../../components/CarCard";
+import CarShowcase from "../../components/CarShowcase";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 export default function Home() {
-  const cars = [
-    {
-      id: 1,
-      img: "https://cdn.motor1.com/images/mgl/ZnKvO2/s3/2023-porsche-911-carrera-t-in-gulf-blue.webp",
-      name: "Porsche 911",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...",
-      postedBy: "Steiner",
-      kilometers: 0,
-      year: 2020,
-      price: 1300000,
-    },
-    {
-      id: 2,
-      img: "https://media.cdnws.com/_i/286143/19770/1635/72/mercedes-amg-gt-black-series.jpeg",
-      name: "Mercedes AMG GT3 Black Series",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...",
-      postedBy: "Enrico",
-      kilometers: 0,
-      year: 2022,
-      price: 1800000,
-    },
-    {
-      id: 3,
-      img: "https://cdn.motor1.com/images/mgl/jbGeN/s3/2023-chevrolet-corvette-z06-front-view.webp",
-      name: "Corvette Z06",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem...",
-      postedBy: "Victor",
-      kilometers: 0,
-      year: 2021,
-      price: 1100000.0,
-    },
-  ];
+  
   return (
     <div className="flex flex-col">
       <Header />
-
-      <ul className="flex gap-4 mt-36">
-        {cars.map((car) => (
-          <CarCard car={car} key={car.id}></CarCard>
-        ))}
-      </ul>
+      <CarShowcase />
       <Footer />
     </div>
   );
